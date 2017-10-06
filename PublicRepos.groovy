4b611eebbcde3ec1c2fd0cf9e39cf6f8bd72056d
Ratnangi Nirek <ratnangi.nirek@dell.com>
2017-10-06 14:25:04 -0500

    Update PublicRepos.groovy

--- PublicRepos.groovy
+++ PublicRepos.groovy
@@ -2,20 +2,6 @@
 
 package com.dell.cpsd.SCM
 
-def getRepoName() {
-    return env.JOB_NAME.split('/')[1]
-}
-
-def getOrgName() {
-    if (env.JOB_NAME ==~ /^dellemc-symphony.*/) {
-        return "dellemc-symphony"
-    } else if (env.JOB_NAME ==~ /^vce-symphony.*/) {
-        return "vce-symphony"
-    } else {
-        return env.JOB_NAME.split('/')[0]
-    }
-}
-
 def getPublicRepos() {
     def publicRepos = []
     def response = new URL("https://api.github.com/orgs/dellemc-symphony/repos?per_page=100000").text
